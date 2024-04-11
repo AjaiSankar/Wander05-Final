3ee1661993d9a476fe30cc8ffdf13a9f364aa909
Ajai Sankar Hareesh <ajaisankarhareesh17@gmail.com>
2024-04-11 16:05:10 +0530

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: LoginPage(),
       routes: {
         '/login': (context) => LoginPage(),
         //'/itinerary': (context) => const Itinerary(),
